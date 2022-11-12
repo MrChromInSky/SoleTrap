168a645bd4887e91ab13d79c5e67987c39657f18
Lukiks <65121528+Lukiks@users.noreply.github.com>
2022-11-12 14:33:26 +0100

    Humen animations and AI basic

--- Project SoleTrap/Assets/Scripts/AI/AIController.cs
+++ Project SoleTrap/Assets/Scripts/AI/AIController.cs
@@ -1,16 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine.Animations;
 using UnityEngine;
 
 public class AIController : MonoBehaviour
 {
+
+    public Transform destiation;
+
+    private NavMeshAgent agent;
+    private Animator anim;
+
     void Start()
     {
-        
+        agent = transform.GetComponent<NavMeshAgent>();
+        anim = transform.GetComponent<Animator>();
     }
 
     void Update()
     {
-        
+        if (transform.position.x != destiation.position.x || transform.position.z != destiation.position.z)
+        {
+            agent.SetDestination(destiation.position);
+            anim.SetBool("walking", true);
+        }
+        else
+            anim.SetBool("walking", false);
     }
 }
